ef2461a5a45cadb3214169dcf129ac11ad4966cf
Viacheslav Kuznetsov <via.kuznetsov@gmail.com>
2021-04-23 15:23:57 +0300

    changing assertion to assert.equal

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,7 +1,7 @@
 import { strict as assert } from 'assert';
 import capitalize from '../src/capitalize.js'
 
-assert(capitalize('') === '');
-assert(capitalize('hello') === 'Hello');
+assert.equal(capitalize(''), '');
+assert.equal(capitalize('hello'), 'Hello');
 
 console.log('Все тесты пройдены!');
